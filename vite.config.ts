6e8123306ce486a7e406af9d9ccb1b5c6ef143ef
r5207644 <sarveshjnv5207644@gmail.com>
2026-02-16 23:12:34 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/abhi-cyber-cafe',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
